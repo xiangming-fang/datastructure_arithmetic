d678aae389476c2f900f5185a77e95b4bd75856d
albert.fang <albert.fang@51job.com>
2020-10-26 14:02:17 +0800

    1、测试数组队列、循环队列、链表队列的效率 2、结论，循环队列和链表队列由于时间复杂度一样，所以相差并不大，但是和数组队列的比较是比较大的。 3、总体来说循环队列和链表队列的效率一样，数组队列性能较差

--- data_structure/src/com/xm/jy/stack/Stacks/Main.java
+++ data_structure/src/com/xm/jy/stack/Stacks/Main.java
@@ -1,10 +1,38 @@
 package com.xm.jy.stack.Stacks;
 
+import com.xm.jy.queue.Queues.ArrayQueue;
+import com.xm.jy.queue.Queues.LinkedListQueue;
+import com.xm.jy.queue.Queues.LoopQueue;
+import com.xm.jy.queue.Queues.Queue;
+
+import java.util.Random;
+
 /**
  * @author: albert.fang
  * @date: 2020/10/22 13:29
  * @description: 主函数，测试用
  */
 public class Main {
-
+    // 测试三种队列的实现方式
+    private static void testQueue(Queue<Integer> queue,int countOp) throws Exception {
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < countOp; i++) {
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < countOp; i++) {
+            queue.dequeue();
+        }
+        long endTime = System.nanoTime();
+        System.out.println((endTime - startTime)/10000000.0);
+    }
+    public static void main(String[] args) throws Exception {
+        int countOp = 100000;
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        testQueue(arrayQueue,countOp);
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        testQueue(loopQueue,countOp);
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        testQueue(linkedListQueue,countOp);
+    }
 }
