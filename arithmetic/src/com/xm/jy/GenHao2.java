a7dae3f9ce5de757993bd9e0476b376a06ef4f6b
albert.fang <albert.fang@51job.com>
2021-04-20 14:24:56 +0800

    完善孤独的根号2

--- arithmetic/src/com/xm/jy/GenHao2.java
+++ arithmetic/src/com/xm/jy/GenHao2.java
@@ -12,17 +12,30 @@ public class GenHao2 {
         return (a * a - num) * (b * b - num) < 0;
     }
 
+    // 求num这个数的根号值
     private static double getGenHaoValue(int num){
         double a = 0;
         double b = num;
         return recursion(a,b,num);
     }
 
+    /**
+     * 递归求解，终止条件 （1）b - a的精度 小于指定值 （2）选择的起始区间中点的函数值刚好等于0
+     * 符合终止条件，直接返回。不符合，将问题划分成更小的子问题，递归执行子问题。
+     * @param a 左区间最边的值
+     * @param b 右区间最边的值
+     * @param num 求num这个数的根号值
+     * @return
+     */
     private static double recursion(double a,double b,int num){
         if (b - a < 0.000001){
             return a;
         }
         double mid = (a + b)/2;
+        // 如果两数的中点，使得函数值正好为0，那么直接返回mid值
+        if ((mid * mid - num) == 0){
+            return mid;
+        }
         if (isMinus(a,mid,num)) {
             return recursion(a,mid,num);
         }
@@ -31,6 +44,7 @@ public class GenHao2 {
 
     public static void main(String[] args) {
         System.out.println(getGenHaoValue(2));
-        System.out.println(getGenHaoValue(3));
+        System.out.println(getGenHaoValue(9));
+        System.out.println(getGenHaoValue(4));
     }
 }
