c0335c39923f42c27ffbcda6a64bb5a8a75bbb2b
albert.fang <albert.fang@51job.com>
2021-09-06 10:01:28 +0800

    avl 加入height、balancedFactor概念 高度、平衡因子

---
+++ data_structure/src/com/xm/jy/test/AVLTest.java
@@ -0,0 +1,30 @@
+package com.xm.jy.test;
+
+import com.xm.jy.avl.AVLTree;
+import com.xm.jy.utils.FileOperatorUtils;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+/**
+ * @ProjectName: datastructure_arithmetic
+ * @Package: com.xm.jy.test
+ * @ClassName: AVLTest
+ * @Author: albert.fang
+ * @Description: avltest
+ * @Date: 2021/8/24 14:57
+ */
+public class AVLTest {
+
+    @Test
+    public void testAVLBalancedFactor(){
+        LinkedList<String> words = FileOperatorUtils.getWords("E:\\IdeaProjects\\datastructure_arithmetic\\data_structure\\pride-and-prejudice.txt");
+        AVLTree<String> avl = new AVLTree<>();
+        for (String word : words) {
+            avl.add(word);
+        }
+        System.out.println(avl.isBST());
+        System.out.println(avl.isBalance());
+
+    }
+}
