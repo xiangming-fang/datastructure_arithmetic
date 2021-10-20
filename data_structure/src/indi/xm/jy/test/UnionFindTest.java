f29c6f5ed98a0b2837288b6fc0c682b0df29d398
albert.fang <albert.fang@51job.com>
2021-10-20 20:31:10 +0800

    递归 实现 路径压缩优化，使高度保持为2

--- data_structure/src/indi/xm/jy/test/UnionFindTest.java
+++ data_structure/src/indi/xm/jy/test/UnionFindTest.java
@@ -44,8 +44,8 @@ public class UnionFindTest {
 //        int size = 100000;
 //        int m = 10000;
 
-        int size = 100000;
-        int m = 100000;
+        int size = 100000000;
+        int m = 100000000;
 
 //        QuickFind quickFind = new QuickFind(size);
 //        System.out.println("QuickFind : " + testUF(quickFind, m) + " s");
@@ -56,14 +56,17 @@ public class UnionFindTest {
 //        UnionFind1 uf1 = new UnionFind1(size);
 //        System.out.println("UnionFind1 : " + testUF(uf1, m) + " s");
 
-        UnionFind3 uf3 = new UnionFind3(size);
-        System.out.println("UnionFind3 : " + testUF(uf3, m) + " s");
-
-        UnionFind4 uf4 = new UnionFind4(size);
-        System.out.println("UnionFind4 : " + testUF(uf4, m) + " s");
-
+//        UnionFind3 uf3 = new UnionFind3(size);
+//        System.out.println("UnionFind3 : " + testUF(uf3, m) + " s");
+//
+//        UnionFind4 uf4 = new UnionFind4(size);
+//        System.out.println("UnionFind4 : " + testUF(uf4, m) + " s");
+//
         UnionFind5 uf5 = new UnionFind5(size);
         System.out.println("UnionFind5 : " + testUF(uf5, m) + " s");
+
+        UnionFind6 uf6 = new UnionFind6(size);
+        System.out.println("UnionFind6 : " + testUF(uf6, m) + " s");
 
 
     }
---
+++ data_structure/src/indi/xm/jy/unionfind/UnionFind6.java
@@ -0,0 +1,72 @@
+package indi.xm.jy.unionfind;
+
+/**
+ * @ProjectName: datastructure_arithmetic
+ * @Package: indi.xm.jy.unionfind
+ * @ClassName: UnionFind4
+ * @Author: albert.fang
+ * @Description: 基于路径压缩优化 -> 将高度压缩成 2
+ * @Date: 2021/10/20 19:54
+ */
+public class UnionFind6 implements UF{
+
+    // 索引表示节点id
+    // value 表示该节点的父节点id
+    private int[] parent;
+
+    // sz[i] 表示以i为根节点的集合中元素个数
+    private int[] rank;
+
+    public UnionFind6(int size){
+
+        parent = new int[size];
+        rank = new int[size];
+
+        // 初始化的时候，每个节点指向自身
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    // 找到节点p所在树的根节点
+    // 时间复杂度是 O(h) h 是树的高度
+    private int find(int p){
+        if (p < 0 || p > parent.length){
+            throw new IllegalArgumentException("p is out of bound. ");
+        }
+        if (p != parent[p]){
+            parent[p] = find(parent[p]);
+        }
+        return parent[p];
+    }
+
+    @Override
+    public void unionElements(int p, int q) {
+        if (!isConnected(p,q)){
+            int pRoot = find(p);
+            int qRoot = find(q);
+            if (rank[pRoot] < rank[qRoot]){
+                parent[pRoot] = qRoot;
+            }
+            else if (rank[pRoot] > rank[qRoot]){
+                parent[qRoot] = pRoot;
+            }
+            else {
+                parent[qRoot] = pRoot;
+                rank[pRoot] += 1;
+            }
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return parent.length;
+    }
+}
