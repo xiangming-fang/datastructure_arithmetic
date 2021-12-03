594f312789d3d44e970ca58b095cc2dac50e3a1d
albert.fang <albert.fang@51job.com>
2021-12-03 14:47:07 +0800

    将无序数组堆化，从最后一个开始，不断执行siftdown操作

--- interview_question/src/indi/xm/jy/heap/MaxHeap.java
+++ interview_question/src/indi/xm/jy/heap/MaxHeap.java
@@ -1,5 +1,7 @@
 package indi.xm.jy.heap;
 
+import indi.xm.jy.utils.ArrayUtil;
+
 import java.util.PriorityQueue;
 import java.util.Random;
 
@@ -22,6 +24,14 @@ public class MaxHeap {
         this.size = 0;
     }
 
+    // 将传入的数组进行堆化
+    public MaxHeap(int[] arr){
+        this.arr = new int[arr.length];
+        System.arraycopy(arr,0,this.arr,0,arr.length);
+        heapify();
+        this.size = arr.length;
+    }
+
     public int getSize(){
         return size;
     }
@@ -32,12 +42,12 @@ public class MaxHeap {
             throw new RuntimeException("堆空间不足，不能添加");
         }
         arr[size] = value;
-        siftUp(size);
+        siftUp(arr,size);
         size ++;
     }
 
     // 元素上移
-    private void siftUp(int index){
+    private void siftUp(int[] arr,int index){
         // 当前index不是堆顶 && 父节点的值小于当前节点 就要执行与父节点交换操作
         while (index != 0 && arr[parentIndex(index)] < arr[index]){
             int parentIndex = parentIndex(index);
@@ -53,12 +63,12 @@ public class MaxHeap {
         }
         int res = arr[0];
         swap(0,--size);
-        siftDown(0);
+        siftDown(arr,size,0);
         return res;
     }
 
     // 元素下移
-    private void siftDown(int index){
+    private void siftDown(int[] arr,int size,int index){
         while (index < size && leftChildIndex(index) < size){
             int leftChildIndex = leftChildIndex(index);
             int rightChildIndex =  -1;
@@ -78,6 +88,13 @@ public class MaxHeap {
                 swap(index,pendIndex);
                 index = pendIndex;
             }else {break;}
+        }
+    }
+
+    // 堆化
+    private void heapify(){
+        for (int i = this.arr.length - 1; i >= 0; i--) {
+            siftDown(this.arr,this.arr.length,i);
         }
     }
 
@@ -115,12 +132,22 @@ public class MaxHeap {
 
 class MaxHeapTest{
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            if (!detector(10000)) {
-                throw new RuntimeException("生成最大堆失败");
+
+//        for (int i = 0; i < 100; i++) {
+//            if (!detector(1000)) {
+//                throw new RuntimeException("生成最大堆失败");
+//            }
+//        }
+//        System.out.println("全部比较成功");
+
+
+        // maxHeap 的heapify
+        for (int i = 0; i < 100000; i++) {
+            if (!detectorHeapify()) {
+                throw new RuntimeException("输入数组 heapify 失败");
             }
         }
-        System.out.println("全部比较成功");
+        System.out.println("全部 heapify 成功");
     }
 
     private static boolean detector(int size){
@@ -133,6 +160,24 @@ class MaxHeapTest{
             pq.offer(num);
         }
         for (int i = 0; i < size; i++) {
+            if (pq.poll() != maxHeap.poll()){
+                System.out.println(i);
+                return false;
+            }
+        }
+        // 这里是判断自己实现的大根堆移除之后的size 和 pq size 是否一样
+        // 如果不一样，说明maxHeap 的size 维护错误
+        return maxHeap.getSize() == pq.size();
+    }
+
+    private static boolean detectorHeapify(){
+        int[] ints = ArrayUtil.generatorArray();
+        MaxHeap maxHeap = new MaxHeap(ints);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+        for (int anInt : ints) {
+            pq.offer(anInt);
+        }
+        for (int i = 0; i < ints.length; i++) {
             if (pq.poll() != maxHeap.poll()){
                 System.out.println(i);
                 return false;
